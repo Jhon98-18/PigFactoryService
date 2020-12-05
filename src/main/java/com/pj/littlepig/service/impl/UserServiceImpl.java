e8998e704b2da99ba5b7cdb704af60d1ff17a3bd
Denial <1798805612@qq.com>
2020-12-05 16:18:40 +0800

    用户注册接口提交

--- src/main/java/com/pj/littlepig/service/impl/UserServiceImpl.java
+++ src/main/java/com/pj/littlepig/service/impl/UserServiceImpl.java
@@ -4,6 +4,7 @@ import com.pj.littlepig.dao.UserMapper;
 import com.pj.littlepig.pojo.SysUser;
 import com.pj.littlepig.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
@@ -26,9 +27,26 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public void insert(SysUser user) {
-        if (!StringUtils.isEmpty(user)) {
-            //TODO：此处需要加主键和密码加密
+        boolean isPass = preInsertUser(user);
+        if (isPass) {
+            BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+            user.setPassWord(bCryptPasswordEncoder.encode(user.getPassWord()));
             int insert = userMapper.insert(user);
         }
+    }
+
+    private boolean preInsertUser(SysUser user) {
+        if (user == null) {
+            throw new RuntimeException("用户不能为空");
+        }
+
+        if (user.getPassWord() == null) {
+            throw new RuntimeException("密码为空!!");
+        }
+        SysUser sysUser = userMapper.selectByName(user.getUserName());
+        if (sysUser != null) {
+            throw new RuntimeException("用户重复！");
+        }
+        return true;
     }
 }
